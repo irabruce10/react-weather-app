8a1655726034b93f8fecd431ff359859b1e3a1ed
Bruce <irabruce10@gmail.com>
2024-09-21 11:22:13 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const [weather, setWeather] = useState([]);
+  const city = 'linz'; // Replace with your desired city name.
+
+  const API_KEY = 'd76570719fb98cbfdad6e5f2fb907ddd';
+
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+  const getWeather = async function () {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    setWeather(data);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     <div>
       <h1>Hello </h1>
+
+      {weather.name}
+
+      {weather.weather.map((weather, index) => (
+        <p key={index}>
+          {weather.description} - {weather.main.temp}°C
+        </p>
+      ))}
     </div>
   );
 }
