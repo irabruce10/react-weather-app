ad64a16ef280152e563e9e9baa9c274fb4a1a4ac
Bruce <irabruce10@gmail.com>
2024-09-23 08:31:22 +0200

    Adding UI

--- src/App.js
+++ src/App.js
@@ -9,24 +9,43 @@ export default function App() {
   const API_KEY = 'd76570719fb98cbfdad6e5f2fb907ddd';
 
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-  const fetchWeatherData = async () => {
+  // const fetchWeatherData = async () => {
+  //   try {
+  //     const response = await axios.get(url);
+  //     setWeatherData(response.data);
+  //     console.log(response.data);
+
+  //     console.log(weatherData);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  const getData = async () => {
     try {
       const response = await axios.get(url);
-      setWeatherData(response.data);
-      console.log(response.data);
 
-      console.log(weatherData);
+      setWeatherData(response.data);
+      7;
+      console.log(response.data);
     } catch (error) {
       console.log(error);
     }
   };
-  fetchWeatherData();
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const submitHandle = (event) => {
     event.preventDefault();
-    console.log('done');
-    fetchWeatherData();
+    getData();
+
+    console.log(weatherData);
   };
+
+  // if (!weatherData) {
+  //   return <div>Loading.....</div>;
+  // }
   return (
     <div>
       <h1>Hello React Weather App </h1>
@@ -38,12 +57,14 @@ export default function App() {
           onChange={(e) => setCity(e.target.value)}
         />
       </form>
-      {weatherData && (
+      {weatherData ? (
         <div>
           <h2>{weatherData.name}</h2>
           <p>{weatherData.weather[0].description}</p>
           <p>Temperature: {weatherData.main.temp}</p>
         </div>
+      ) : (
+        <div>LOADING...</div>
       )}
     </div>
   );
